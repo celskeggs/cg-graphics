5789ab53f60b48a42027569e7d9b9b9fa0beca67
Colby Skeggs <git@colbyskeggs.com>
2015-10-12 22:17:20 -0700

    Refactored out keys.py

--- graphics.py
+++ graphics.py
@@ -9,7 +9,7 @@ see http://creativecommons.org/licenses/by-nc-sa/3.0/ for details
 
 print "using graphics.py library version 3.8"
 
-import pygame, os, math, colors
+import pygame, os, math, colors, keys
 
 class World:
     pass
@@ -108,15 +108,6 @@ class GameLibInfo:
                 print getActualFrameRate()
                 self.FPStime = time
                 self.FPScount = 0
-
-    def loadKeys(self, keyList):
-        self.keyList = keyList
-        self.key2nameDict = dict()
-        self.name2keyDict = dict()
-        for (code, nameList) in keyList:
-            self.key2nameDict[code] = nameList[0].lower()
-            for name in nameList:
-                self.name2keyDict[name.lower()] = code
 
 _GLI = GameLibInfo()
 
@@ -395,21 +386,14 @@ def isKeyPressed(key):
     return _GLI.keysPressedNow.get(key, False)
 
 def getKeyName(key):
-    if key in _GLI.key2nameDict:
-        return _GLI.key2nameDict[key]
-    else:
-        return None
+    return keys.key2nameDict.get(key, None)
 
 def getKeyCode(key):
     if key is None:
         return None
-    if key in _GLI.key2nameDict:
+    if key in keys.key2nameDict:
         return key
-    key = key.lower()
-    if key in _GLI.name2keyDict:
-        return _GLI.name2keyDict[key]
-    else:
-        return None
+    return keys.name2keyDict.get(key.lower(), None)
 
 def sameKeys(key1, key2):
     code1 = getKeyCode(key1)
@@ -623,153 +607,6 @@ def resetTime():
 
 def setFrameRate(frameRate):
     _GLI.frameRate = frameRate
-
-#########################################################
-
-def makeKeysWebPage():
-    web = file("keys.html", "w")
-    web.write("""<html><head><title>Python Keys</title></head>
-                 <body><center>
-                 <h1>Key Names</h1>
-                 <table>
-              """)
-    count = 0
-
-    for (code, nameList) in _GLI.keyList:
-        web.write('<tr>')
-        for name in nameList:
-            web.write('<td>' + name + '</td>')
-        web.write('</tr>')
-    web.write('</table></center></body></html>')
-    web.close()
-
-###################################################################
-
-_GLI.loadKeys([
-    (pygame.K_UP, ['up','up arrow']),
-    (pygame.K_DOWN, ['down','down arrow']),
-    (pygame.K_RIGHT, ['right','right arrow']),
-    (pygame.K_LEFT, ['left','left arrow']),
-    (pygame.K_BACKSPACE, ['backspace']),
-    (pygame.K_SPACE, ['space', ' ']),
-    (pygame.K_RETURN, ['enter', 'return']),
-    (pygame.K_TAB, ['tab']),
-
-    (pygame.K_a, ['a']),
-    (pygame.K_b, ['b']),
-    (pygame.K_c, ['c']),
-    (pygame.K_d, ['d']),
-    (pygame.K_e, ['e']),
-    (pygame.K_f, ['f']),
-    (pygame.K_g, ['g']),
-    (pygame.K_h, ['h']),
-    (pygame.K_i, ['i']),
-    (pygame.K_j, ['j']),
-    (pygame.K_k, ['k']),
-    (pygame.K_l, ['l']),
-    (pygame.K_m, ['m']),
-    (pygame.K_n, ['n']),
-    (pygame.K_o, ['o']),
-    (pygame.K_p, ['p']),
-    (pygame.K_q, ['q']),
-    (pygame.K_r, ['r']),
-    (pygame.K_s, ['s']),
-    (pygame.K_t, ['t']),
-    (pygame.K_u, ['u']),
-    (pygame.K_v, ['v']),
-    (pygame.K_w, ['w']),
-    (pygame.K_x, ['x']),
-    (pygame.K_y, ['y']),
-    (pygame.K_z, ['z']),
-    (pygame.K_0, ['0']),
-    (pygame.K_1, ['1']),
-    (pygame.K_2, ['2']),
-    (pygame.K_3, ['3']),
-    (pygame.K_4, ['4']),
-    (pygame.K_5, ['5']),
-    (pygame.K_6, ['6']),
-    (pygame.K_7, ['7']),
-    (pygame.K_8, ['8']),
-    (pygame.K_9, ['9']),
-
-    (pygame.K_BACKQUOTE, ['`' ,'backquote', 'grave', 'grave accent']),
-    (pygame.K_MINUS, ['-','minus','dash','hyphen']),
-    (pygame.K_EQUALS, ['=','equals']),
-    (pygame.K_LEFTBRACKET, ['[','left bracket']),
-    (pygame.K_RIGHTBRACKET, [']','right bracket']),
-    (pygame.K_BACKSLASH, ['backslash', '\\']),
-    (pygame.K_SEMICOLON, [';','semicolon']),
-    (pygame.K_QUOTE, ['quote', '\'']),
-    (pygame.K_COMMA, [',','comma']),
-    (pygame.K_PERIOD, ['.','period']),
-    (pygame.K_SLASH, ['/','slash','divide']),
-
-    (pygame.K_DELETE, ['delete']),
-    (pygame.K_INSERT, ['insert']),
-    (pygame.K_HOME, ['home']),
-    (pygame.K_END, ['end']),
-    (pygame.K_PAGEUP, ['page up']),
-    (pygame.K_PAGEDOWN, ['page down']),
-    (pygame.K_CLEAR, ['clear']),
-    (pygame.K_PAUSE, ['pause']),
-
-    (pygame.K_F1, ['F1']),
-    (pygame.K_F2, ['F2']),
-    (pygame.K_F3, ['F3']),
-    (pygame.K_F4, ['F4']),
-    (pygame.K_F5, ['F5']),
-    (pygame.K_F6, ['F6']),
-    (pygame.K_F7, ['F7']),
-    (pygame.K_F8, ['F8']),
-    (pygame.K_F9, ['F9']),
-    (pygame.K_F10, ['F10']),
-    (pygame.K_F11, ['F11']),
-    (pygame.K_F12, ['F12']),
-    (pygame.K_F13, ['F13']),
-    (pygame.K_F14, ['F14']),
-    (pygame.K_F15, ['F15']),
-
-    (pygame.K_RSHIFT, ['right shift']),
-    (pygame.K_LSHIFT, ['left shift']),
-    (pygame.K_RCTRL, ['right ctrl']),
-    (pygame.K_LCTRL, ['left ctrl']),
-    (pygame.K_RALT, ['right alt', 'right option']),
-    (pygame.K_LALT, ['left alt', 'left option']),
-    (pygame.K_RMETA, ['right command']),
-    (pygame.K_LMETA, ['left command']),
-    (pygame.K_LSUPER, ['left windows']),
-    (pygame.K_RSUPER, ['right windows']),
-
-    (pygame.K_NUMLOCK, ['numlock']),
-    (pygame.K_CAPSLOCK, ['capslock']),
-    (pygame.K_SCROLLOCK, ['scrollock']),
-    (pygame.K_MODE, ['mode']),
-    (pygame.K_HELP, ['help']),
-    (pygame.K_PRINT, ['print','print screen','prtsc']),
-    (pygame.K_SYSREQ, ['sysrq']),
-    (pygame.K_BREAK, ['break']),
-    (pygame.K_MENU, ['menu']),
-    (pygame.K_POWER, ['power']),
-    (pygame.K_EURO, ['euro']),
-
-    (pygame.K_KP0, ['keypad 0']),
-    (pygame.K_KP1, ['keypad 1']),
-    (pygame.K_KP2, ['keypad 2']),
-    (pygame.K_KP3, ['keypad 3']),
-    (pygame.K_KP4, ['keypad 4']),
-    (pygame.K_KP5, ['keypad 5']),
-    (pygame.K_KP6, ['keypad 6']),
-    (pygame.K_KP7, ['keypad 7']),
-    (pygame.K_KP8, ['keypad 8']),
-    (pygame.K_KP9, ['keypad 9']),
-    (pygame.K_KP_PERIOD, ['keypad period']),
-    (pygame.K_KP_DIVIDE, ['keypad divide']),
-    (pygame.K_KP_MULTIPLY, ['keypad multiply']),
-    (pygame.K_KP_MINUS, ['keypad minus']),
-    (pygame.K_KP_PLUS, ['keypad plus']),
-    (pygame.K_KP_EQUALS, ['keypad equals']),
-    (pygame.K_KP_ENTER, ['keypad enter'])
-])
 
 ###################################################################
 # Backward Compatibility
