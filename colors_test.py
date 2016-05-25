a7695cc67d23616448c11f1da57c81e710666fac
Cel Skeggs <git@celskeggs.com>
2016-05-25 15:20:39 -0700

    Add basic testing system

---
+++ colors_test.py
@@ -0,0 +1,30 @@
+import graphics, colors, random
+
+expected_colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen']
+
+def test_getColorsList():
+    assert graphics.getColorsList() == expected_colors
+
+def assert_valid_color(x):
+    assert type(x) == tuple and len(x) == 3
+    r, g, b = x
+    assert 0 <= r < 256 and 0 <= g < 256 and 0 <= b < 256
+
+def test_lookupColor_correct():
+    for color in expected_colors:
+        assert_valid_color(graphics.lookupColor(color))
+
+def test_lookupColor_missing():
+    # TODO: is this really what we want?
+    for color in expected_colors:
+        bad = color + "a"
+        assert graphics.lookupColor(bad) == bad
+
+def test_lookupColor_raw():
+    ran = range(1, 255)
+    elems = [0, 255] + [random.choice(ran) for i in range(0,30)]
+    for r in elems:
+        for g in elems:
+            for b in elems:
+                val = "#%.2x%.2x%.2x" % (r, g, b)
+                assert graphics.lookupColor(val) == val
